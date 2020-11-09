ad0ac83958fd7784089fae55592d6cfe43902b05
Crowdbotics <team@crowdbotics.com>
2020-11-09 05:37:33 +0000

    Update store

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,300 @@
 import * as types from "./constants"
+export const api_v1_customtext_list = () => ({
+  type: types.API_V1_CUSTOMTEXT_LIST
+})
+export const api_v1_customtext_listSucceeded = (response, starter) => ({
+  type: types.API_V1_CUSTOMTEXT_LIST_SUCCEEDED,
+  response,
+  starter
+})
+export const api_v1_customtext_listFailed = (error, starter) => ({
+  type: types.API_V1_CUSTOMTEXT_LIST_FAILED,
+  error,
+  starter
+})
+export const api_v1_customtext_read = () => ({
+  type: types.API_V1_CUSTOMTEXT_READ
+})
+export const api_v1_customtext_readSucceeded = (response, starter) => ({
+  type: types.API_V1_CUSTOMTEXT_READ_SUCCEEDED,
+  response,
+  starter
+})
+export const api_v1_customtext_readFailed = (error, starter) => ({
+  type: types.API_V1_CUSTOMTEXT_READ_FAILED,
+  error,
+  starter
+})
+export const api_v1_customtext_update = data => ({
+  type: types.API_V1_CUSTOMTEXT_UPDATE,
+  data
+})
+export const api_v1_customtext_updateSucceeded = (response, starter) => ({
+  type: types.API_V1_CUSTOMTEXT_UPDATE_SUCCEEDED,
+  response,
+  starter
+})
+export const api_v1_customtext_updateFailed = (error, starter) => ({
+  type: types.API_V1_CUSTOMTEXT_UPDATE_FAILED,
+  error,
+  starter
+})
+export const api_v1_customtext_partial_update = data => ({
+  type: types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE,
+  data
+})
+export const api_v1_customtext_partial_updateSucceeded = (
+  response,
+  starter
+) => ({
+  type: types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE_SUCCEEDED,
+  response,
+  starter
+})
+export const api_v1_customtext_partial_updateFailed = (error, starter) => ({
+  type: types.API_V1_CUSTOMTEXT_PARTIAL_UPDATE_FAILED,
+  error,
+  starter
+})
+export const api_v1_homepage_list = () => ({ type: types.API_V1_HOMEPAGE_LIST })
+export const api_v1_homepage_listSucceeded = (response, starter) => ({
+  type: types.API_V1_HOMEPAGE_LIST_SUCCEEDED,
+  response,
+  starter
+})
+export const api_v1_homepage_listFailed = (error, starter) => ({
+  type: types.API_V1_HOMEPAGE_LIST_FAILED,
+  error,
+  starter
+})
+export const api_v1_homepage_read = () => ({ type: types.API_V1_HOMEPAGE_READ })
+export const api_v1_homepage_readSucceeded = (response, starter) => ({
+  type: types.API_V1_HOMEPAGE_READ_SUCCEEDED,
+  response,
+  starter
+})
+export const api_v1_homepage_readFailed = (error, starter) => ({
+  type: types.API_V1_HOMEPAGE_READ_FAILED,
+  error,
+  starter
+})
+export const api_v1_homepage_update = data => ({
+  type: types.API_V1_HOMEPAGE_UPDATE,
+  data
+})
+export const api_v1_homepage_updateSucceeded = (response, starter) => ({
+  type: types.API_V1_HOMEPAGE_UPDATE_SUCCEEDED,
+  response,
+  starter
+})
+export const api_v1_homepage_updateFailed = (error, starter) => ({
+  type: types.API_V1_HOMEPAGE_UPDATE_FAILED,
+  error,
+  starter
+})
+export const api_v1_homepage_partial_update = data => ({
+  type: types.API_V1_HOMEPAGE_PARTIAL_UPDATE,
+  data
+})
+export const api_v1_homepage_partial_updateSucceeded = (response, starter) => ({
+  type: types.API_V1_HOMEPAGE_PARTIAL_UPDATE_SUCCEEDED,
+  response,
+  starter
+})
+export const api_v1_homepage_partial_updateFailed = (error, starter) => ({
+  type: types.API_V1_HOMEPAGE_PARTIAL_UPDATE_FAILED,
+  error,
+  starter
+})
+export const api_v1_login_create = () => ({ type: types.API_V1_LOGIN_CREATE })
+export const api_v1_login_createSucceeded = (response, starter) => ({
+  type: types.API_V1_LOGIN_CREATE_SUCCEEDED,
+  response,
+  starter
+})
+export const api_v1_login_createFailed = (error, starter) => ({
+  type: types.API_V1_LOGIN_CREATE_FAILED,
+  error,
+  starter
+})
+export const api_v1_signup_create = data => ({
+  type: types.API_V1_SIGNUP_CREATE,
+  data
+})
+export const api_v1_signup_createSucceeded = (response, starter) => ({
+  type: types.API_V1_SIGNUP_CREATE_SUCCEEDED,
+  response,
+  starter
+})
+export const api_v1_signup_createFailed = (error, starter) => ({
+  type: types.API_V1_SIGNUP_CREATE_FAILED,
+  error,
+  starter
+})
+export const rest_auth_login_create = data => ({
+  type: types.REST_AUTH_LOGIN_CREATE,
+  data
+})
+export const rest_auth_login_createSucceeded = (response, starter) => ({
+  type: types.REST_AUTH_LOGIN_CREATE_SUCCEEDED,
+  response,
+  starter
+})
+export const rest_auth_login_createFailed = (error, starter) => ({
+  type: types.REST_AUTH_LOGIN_CREATE_FAILED,
+  error,
+  starter
+})
+export const rest_auth_logout_list = () => ({
+  type: types.REST_AUTH_LOGOUT_LIST
+})
+export const rest_auth_logout_listSucceeded = (response, starter) => ({
+  type: types.REST_AUTH_LOGOUT_LIST_SUCCEEDED,
+  response,
+  starter
+})
+export const rest_auth_logout_listFailed = (error, starter) => ({
+  type: types.REST_AUTH_LOGOUT_LIST_FAILED,
+  error,
+  starter
+})
+export const rest_auth_logout_create = () => ({
+  type: types.REST_AUTH_LOGOUT_CREATE
+})
+export const rest_auth_logout_createSucceeded = (response, starter) => ({
+  type: types.REST_AUTH_LOGOUT_CREATE_SUCCEEDED,
+  response,
+  starter
+})
+export const rest_auth_logout_createFailed = (error, starter) => ({
+  type: types.REST_AUTH_LOGOUT_CREATE_FAILED,
+  error,
+  starter
+})
+export const rest_auth_password_change_create = data => ({
+  type: types.REST_AUTH_PASSWORD_CHANGE_CREATE,
+  data
+})
+export const rest_auth_password_change_createSucceeded = (
+  response,
+  starter
+) => ({
+  type: types.REST_AUTH_PASSWORD_CHANGE_CREATE_SUCCEEDED,
+  response,
+  starter
+})
+export const rest_auth_password_change_createFailed = (error, starter) => ({
+  type: types.REST_AUTH_PASSWORD_CHANGE_CREATE_FAILED,
+  error,
+  starter
+})
+export const rest_auth_password_reset_create = data => ({
+  type: types.REST_AUTH_PASSWORD_RESET_CREATE,
+  data
+})
+export const rest_auth_password_reset_createSucceeded = (
+  response,
+  starter
+) => ({
+  type: types.REST_AUTH_PASSWORD_RESET_CREATE_SUCCEEDED,
+  response,
+  starter
+})
+export const rest_auth_password_reset_createFailed = (error, starter) => ({
+  type: types.REST_AUTH_PASSWORD_RESET_CREATE_FAILED,
+  error,
+  starter
+})
+export const rest_auth_password_reset_confirm_create = data => ({
+  type: types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE,
+  data
+})
+export const rest_auth_password_reset_confirm_createSucceeded = (
+  response,
+  starter
+) => ({
+  type: types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE_SUCCEEDED,
+  response,
+  starter
+})
+export const rest_auth_password_reset_confirm_createFailed = (
+  error,
+  starter
+) => ({
+  type: types.REST_AUTH_PASSWORD_RESET_CONFIRM_CREATE_FAILED,
+  error,
+  starter
+})
+export const rest_auth_registration_create = data => ({
+  type: types.REST_AUTH_REGISTRATION_CREATE,
+  data
+})
+export const rest_auth_registration_createSucceeded = (response, starter) => ({
+  type: types.REST_AUTH_REGISTRATION_CREATE_SUCCEEDED,
+  response,
+  starter
+})
+export const rest_auth_registration_createFailed = (error, starter) => ({
+  type: types.REST_AUTH_REGISTRATION_CREATE_FAILED,
+  error,
+  starter
+})
+export const rest_auth_registration_verify_email_create = data => ({
+  type: types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE,
+  data
+})
+export const rest_auth_registration_verify_email_createSucceeded = (
+  response,
+  starter
+) => ({
+  type: types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE_SUCCEEDED,
+  response,
+  starter
+})
+export const rest_auth_registration_verify_email_createFailed = (
+  error,
+  starter
+) => ({
+  type: types.REST_AUTH_REGISTRATION_VERIFY_EMAIL_CREATE_FAILED,
+  error,
+  starter
+})
+export const rest_auth_user_read = () => ({ type: types.REST_AUTH_USER_READ })
+export const rest_auth_user_readSucceeded = (response, starter) => ({
+  type: types.REST_AUTH_USER_READ_SUCCEEDED,
+  response,
+  starter
+})
+export const rest_auth_user_readFailed = (error, starter) => ({
+  type: types.REST_AUTH_USER_READ_FAILED,
+  error,
+  starter
+})
+export const rest_auth_user_update = data => ({
+  type: types.REST_AUTH_USER_UPDATE,
+  data
+})
+export const rest_auth_user_updateSucceeded = (response, starter) => ({
+  type: types.REST_AUTH_USER_UPDATE_SUCCEEDED,
+  response,
+  starter
+})
+export const rest_auth_user_updateFailed = (error, starter) => ({
+  type: types.REST_AUTH_USER_UPDATE_FAILED,
+  error,
+  starter
+})
+export const rest_auth_user_partial_update = data => ({
+  type: types.REST_AUTH_USER_PARTIAL_UPDATE,
+  data
+})
+export const rest_auth_user_partial_updateSucceeded = (response, starter) => ({
+  type: types.REST_AUTH_USER_PARTIAL_UPDATE_SUCCEEDED,
+  response,
+  starter
+})
+export const rest_auth_user_partial_updateFailed = (error, starter) => ({
+  type: types.REST_AUTH_USER_PARTIAL_UPDATE_FAILED,
+  error,
+  starter
+})
--- src/store/services.js
+++ src/store/services.js
@@ -1,1 +1,115 @@
 import axios from "axios"
+const zebasBkkpandTaxAPI = axios.create({
+  baseURL: "https://zebas-bkkp-and-tax-22430-prod.herokuapp.com/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function api_v1_customtext_list(action) {
+  return zebasBkkpandTaxAPI.get(`/api/v1/customtext/`)
+}
+function api_v1_customtext_read(action) {
+  return zebasBkkpandTaxAPI.get(`/api/v1/customtext/${action.id}/`)
+}
+function api_v1_customtext_update(action) {
+  return zebasBkkpandTaxAPI.put(`/api/v1/customtext/${action.id}/`, null, {
+    data: action.data
+  })
+}
+function api_v1_customtext_partial_update(action) {
+  return zebasBkkpandTaxAPI.patch(`/api/v1/customtext/${action.id}/`, null, {
+    data: action.data
+  })
+}
+function api_v1_homepage_list(action) {
+  return zebasBkkpandTaxAPI.get(`/api/v1/homepage/`)
+}
+function api_v1_homepage_read(action) {
+  return zebasBkkpandTaxAPI.get(`/api/v1/homepage/${action.id}/`)
+}
+function api_v1_homepage_update(action) {
+  return zebasBkkpandTaxAPI.put(`/api/v1/homepage/${action.id}/`, null, {
+    data: action.data
+  })
+}
+function api_v1_homepage_partial_update(action) {
+  return zebasBkkpandTaxAPI.patch(`/api/v1/homepage/${action.id}/`, null, {
+    data: action.data
+  })
+}
+function api_v1_login_create(action) {
+  return zebasBkkpandTaxAPI.post(`/api/v1/login/`)
+}
+function api_v1_signup_create(action) {
+  return zebasBkkpandTaxAPI.post(`/api/v1/signup/`, null, { data: action.data })
+}
+function rest_auth_login_create(action) {
+  return zebasBkkpandTaxAPI.post(`/rest-auth/login/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_logout_list(action) {
+  return zebasBkkpandTaxAPI.get(`/rest-auth/logout/`)
+}
+function rest_auth_logout_create(action) {
+  return zebasBkkpandTaxAPI.post(`/rest-auth/logout/`)
+}
+function rest_auth_password_change_create(action) {
+  return zebasBkkpandTaxAPI.post(`/rest-auth/password/change/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_password_reset_create(action) {
+  return zebasBkkpandTaxAPI.post(`/rest-auth/password/reset/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_password_reset_confirm_create(action) {
+  return zebasBkkpandTaxAPI.post(`/rest-auth/password/reset/confirm/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_registration_create(action) {
+  return zebasBkkpandTaxAPI.post(`/rest-auth/registration/`, null, {
+    data: action.data
+  })
+}
+function rest_auth_registration_verify_email_create(action) {
+  return zebasBkkpandTaxAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    null,
+    { data: action.data }
+  )
+}
+function rest_auth_user_read(action) {
+  return zebasBkkpandTaxAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(action) {
+  return zebasBkkpandTaxAPI.put(`/rest-auth/user/`, null, { data: action.data })
+}
+function rest_auth_user_partial_update(action) {
+  return zebasBkkpandTaxAPI.patch(`/rest-auth/user/`, null, {
+    data: action.data
+  })
+}
+export const apiService = {
+  api_v1_customtext_list,
+  api_v1_customtext_read,
+  api_v1_customtext_update,
+  api_v1_customtext_partial_update,
+  api_v1_homepage_list,
+  api_v1_homepage_read,
+  api_v1_homepage_update,
+  api_v1_homepage_partial_update,
+  api_v1_login_create,
+  api_v1_signup_create,
+  rest_auth_login_create,
+  rest_auth_logout_list,
+  rest_auth_logout_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_create,
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_read,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
+}
